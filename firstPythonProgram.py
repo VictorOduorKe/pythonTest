ae2538d1b3aad0fb3b9d554fdd9e6503fad4f2df
VickypaulKenya <victoroduorxh2@gmail.com>
2025-02-11 12:31:37 +0300

    adding my first python file

---
+++ firstPythonProgram.py
@@ -0,0 +1,91 @@
+#building a simple hut
+print("       *"         )
+print("     * * *"       )
+print("    * * * *"      )
+print("   *** * * **"    )
+print("  ***      ***"   )
+print("    *      *"      )
+print("    *      *"     )
+print("    *******"    )
+
+print(2*8)
+
+#printing hexadecimal
+print(0X1A3*10)
+print(3*10E10)
+
+#printing exponential
+print(3.6898789E-34)
+print(0.0000000000000000000001)
+
+#adding quotes using escape \
+print('\"This is a quote\"')
+print("Am told 2 is greater than 4. What is your view\n"," That stetement is probaly",2>4)
+print("\"I\'m\"")
+print("\"\"learning\"\"")
+print("\"\"\"python\"\"\"")
+
+#calculating salary and display total balance by 4
+var = 4
+client_name="James Kaluki"
+account_bal=40000
+var=var+1
+print(client_name, "You have a balance of ksh", var*account_bal)
+pay=200
+pay2=300+200
+print(pay2)
+
+#finding hypotenoues usign Pythagorus Theorem
+h=9
+b=5
+h=(h**2 + b**2)**0.5
+print ("The value of h= ",h)
+a=3
+b=4
+c=(a**2 +b**2)**0.5
+print(c)
+
+#assigning values to variables and computing thir sum
+john=3
+mary=5
+adam=6
+print(john,",",mary,",",adam)
+total_apples=john+mary+adam
+print("Sum of apples is ",total_apples)
+x=3
+x=x**2
+print(x)
+
+#converting miles to kilometers
+rem = 608 % 10
+print(rem)
+j=4
+j**=3
+print(j)
+mile=1.66
+km=mile
+print("3 miles = ",round(mile*3,2),"km")
+print("4km=",round(4/mile,2))
+
+#solving cubic equation
+#given values
+#x=0
+#x=1
+#x=-1"
+#y=3x^3-2x^2+3x-1
+x=0
+y=float(x)
+y=3*x**3 -2*x**2 +3*x-1
+print("Y=",y)
+
+x=1
+y=float(x)
+y=3*x**3 -2*x**2 +3*x-1
+print("Y=",y)
+
+x=-1
+y=float(x)
+y=3*x**3 -2*x**2 +3*x-1
+print("Y=",y)
+print(9%6%2)
+print(2**2**3)
